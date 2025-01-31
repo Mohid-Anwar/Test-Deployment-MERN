dc324818cf2d7fa1e9c856bae9a0cd45b1ee76af
Mohid-Anwar <mohid.anwar@gmail.com>
2025-01-31 18:59:40 +0500

    AGAIN CHANGES

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -2,6 +2,15 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
-export default axios.create({
-  baseURL: `${baseURL}/api`,
+// Create axios instance with the full server URL
+const instance = axios.create({
+  baseURL, // Don't append /api here
 });
+
+// Add a request interceptor to log the full URL (for debugging)
+instance.interceptors.request.use((config) => {
+  console.log('Making request to:', config.baseURL + config.url);
+  return config;
+});
+
+export default instance;

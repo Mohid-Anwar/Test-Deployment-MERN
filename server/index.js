53946d20245de38f4632f727bdf4ab868d8c6892
Mohid-Anwar <mohid.anwar@gmail.com>
2025-01-31 18:56:44 +0500

    Made Changes to Vercel and CORs

--- server/index.js
+++ server/index.js
@@ -4,22 +4,21 @@ const axios = require("./config/axiosConfig");
 const dataRoutes = require("./routes/dataRoutes");
 
 const app = express();
-app.use(cors());
-const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: ["https://test-deployment-mern-frontend.vercel.app"],
+// Single CORS configuration
+app.use(cors({
+    origin: ["https://test-deployment-mern-frontend.vercel.app", "http://localhost:5173"],
     methods: ["POST", "GET"],
     credentials: true,
-  })
-);
+}));
+
 app.use(express.json());
 app.use("/api/data", dataRoutes);
 app.get("/", (req, res) => {
-  res.send("Hi I am Working");
+    res.send("Hi I am Working");
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
